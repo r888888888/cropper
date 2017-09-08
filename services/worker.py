c212704a0199937a3dddcb67218fd3c893e9770d
Albert Yi <ayi@desk.com>
2017-09-08 14:28:15 -0700

    fixes

--- services/worker.py
+++ services/worker.py
@@ -7,16 +7,26 @@ import tempfile
 import time
 import requests
 import os
-import urlparse
+import urllib
+from contextlib import closing
+import hmac
+import base64
+import hashlib
 
 load_dotenv(find_dotenv())
 queue_url = os.environ.get("AWS_SQS_URL")
 sqs = boto3.client("sqs")
 loop = True
+thumbor_key = open("/etc/thumbor.key", "r").read().encode()
+
+def build_hmac(x):
+  return base64.urlsafe_b64encode(hmac.new(thumbor_key, x.encode(), digestmod=hashlib.sha1).digest()).decode("utf-8")
 
 def build_thumbor_url(url, width, height):
   url = re.sub(r"^https?://", "", url)
-  return "http://127.0.0.1/{}x{}/smart/{}".format(width, height, url)
+  path = "{}x{}/smart/{}".format(width, height, url)
+  hmac = build_hmac(path)
+  return "http://127.0.0.1:8888/{}/{}".format(hmac, dim, path)
 
 def upload_to_s3(file, key):
   s3 = boto3.client("s3")
@@ -27,11 +37,13 @@ def download_and_process(url):
   ext = os.path.splitext(url)[1].lower()
   small_url = build_thumbor_url(url, 200, 200)
   large_url = build_thumbor_url(url, 640, 320)
-  small_file = tempfile.NamedTemporaryFile("w+b", suffix=ext)
-  large_file = tempfile.NamedTemporaryFile("w+b", suffix=ext)
+  print("small", small_url)
+  print("large", large_url)
+  small_file = tempfile.NamedTemporaryFile("w+b", suffix="jpg")
+  large_file = tempfile.NamedTemporaryFile("w+b", suffix="jpg")
   for (url, file) in [(small_url, small_file), (large_url, large_file)]:
     with closing(requests.get(url, stream=True)) as resp:
-      for chunk in resp.iter_Content(chunk_size=None):
+      for chunk in resp.iter_content(chunk_size=None):
         if chunk:
           file.write(chunk)
   return (small_file, large_file)
@@ -43,7 +55,7 @@ while loop:
       for message in response["Messages"]:
         receipt_handle = message["ReceiptHandle"]
         post_id, url = message["Body"].split(",")
-        filename = os.path.basename(urlparse.urlparse(url).path)
+        filename = os.path.basename(urllib.parse.urlparse(url).path)
         print("processing", post_id)
         small_file, large_file = download_and_process(url)
         upload_to_s3(small_file, "cropped/small/{}".format(filename))
